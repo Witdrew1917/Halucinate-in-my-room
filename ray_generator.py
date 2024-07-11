21b7d56b95b36c5c273cde2eba0728734c6a813a
vanHalen <arvid.ll@live.se>
2024-07-11 11:28:42 +0200

    Fixed error in ray creation by separating ray into files per image

--- ray_generator.py
+++ ray_generator.py
@@ -20,36 +20,35 @@ def virtual_camera(pixel_width: int, pixel_height:int) -> np.ndarray:
             - origin.reshape(1,1,3)
 
     norm = np.linalg.norm(camera_matrix, axis=1, keepdims=True)
-    print(norm)
+    tol = 1e-03
+    norm[norm < tol] = tol
+
     return camera_matrix / norm
 
 
-def create_rays_synthetic_set(folder_path, transforms: dict):
+def create_rays_synthetic_set(folder_path, frame: dict):
     
     data_set = []
+    file = frame["file_path"].split(".")[1]
 
-    for frame in tqdm(transforms["frames"]):
+    with Image.open(folder_path + file + '.png') as img:
+        pix = img.load()
+        width = img.width
+        height = img.height
 
-        file = frame["file_path"].split(".")[1]
-
-        with Image.open(folder_path + file + '.png') as img:
-            pix = img.load()
-            width = img.width
-            height = img.height
-
-            camera = virtual_camera(width, height)
-            rotation_matrix = np.array(frame["transform_matrix"])
-            ray_matrix = np.matmul(camera, rotation_matrix[:3,:3])
+        camera = virtual_camera(width, height)
+        rotation_matrix = np.array(frame["transform_matrix"])
+        ray_matrix = np.matmul(camera, rotation_matrix[:3,:3])
 
 
-            for y in range(height):
-                for x in range(width):
+        for y in range(height):
+            for x in range(width):
 
-                    sample = {'position': rotation_matrix[:3,3].tolist(),
-                              'direction': ray_matrix[x,y,:].tolist(),
-                              'target':pix[x,y]}
+                sample = {'position': rotation_matrix[:3,3].tolist(),
+                          'direction': ray_matrix[x,y,:].tolist(),
+                          'target':pix[x,y]}
 
-                    data_set.append(sample)
+                data_set.append(sample)
 
     return data_set
     
@@ -61,19 +60,25 @@ if __name__ == '__main__':
     parser.add_argument("--set_type", type=str, default='train')
     args = parser.parse_args()   
 
+    output_dir = './preprocessed_data'
 
     if not args.set_type in ["train", "test", "val"]:
         print("Bad argument, target set must be named train, test or val.")
         sys.exit()
 
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     with open(os.path.join(args.datafolder, \
             f"transforms_{args.set_type}.json")) as json_data:
         transforms = json.load(json_data)
         json_data.close()
-        data_set = create_rays_synthetic_set(args.datafolder, transforms)
-        with open(f"{args.set_type}.pkl", 'wb') as file:
-            pickle.dump(data_set, file=file)
+
+        for id, frame in enumerate(tqdm(transforms["frames"])):
+            data_set = create_rays_synthetic_set(args.datafolder, frame)
+            with open(f"{output_dir}/{args.set_type}_{id}.pkl", 'wb') as file:
+                pickle.dump(data_set, file=file)
+
 
     
 
