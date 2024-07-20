94097f35533dc943b6120f8d35c4e773e86bf613
vanHalen <arvid.ll@live.se>
2024-07-20 21:51:14 +0200

    WIP volume rendering
    Relized that colors, and densities will have dimensions (B,R,D) where B
    is the batch size, R is the granularity of the shot ray, and D is the
    dimension of the color space and density space (i.e. 3 and 1). I think
    that several of the functions (volume rendering) have not taken account
    for this extra dimension (R).

--- utils/positional_encodings.py
+++ utils/positional_encodings.py
@@ -5,7 +5,8 @@ def fourier_encoding(x: torch.Tensor, N: int) -> torch.Tensor:
     '''
         Creates embedding vector using sinusoidal encoding.
         args:
-            x: input vector of shape (d, ) where d is a scalar
+            x: input vector of shape (b,d) where b is the batch size and d is
+            a scalar
             N: dimension of fourier encoding
 
         OBS! Does not handle proper device placement of created tensor.
@@ -16,6 +17,6 @@ def fourier_encoding(x: torch.Tensor, N: int) -> torch.Tensor:
         out.append(torch.sin(2 ** i * pi * x))
         out.append(torch.cos(2 ** i * pi * x))
     
-    return torch.cat(out)
+    return torch.cat(out, dim=-1)
 
 
--- utils/ray_utils.py
+++ utils/ray_utils.py
@@ -1,8 +1,19 @@
 import torch
 
-def create_ray(pos: torch.Tensor, view: torch.Tensor, granularity=0.01):
-    time_scale = torch.arange(0,2,granularity)
+def create_ray(pos: torch.Tensor, view: torch.Tensor, time_len=1,
+               granularity=0.01):
+    time_scale = torch.arange(0,time_len,granularity)
     pos = pos.unsqueeze(-1) + view.unsqueeze(-1) * time_scale
     view = view.unsqueeze(-1).expand(-1,len(time_scale))
     return pos, view
+
+
+def volume_render(color_vec: torch.Tensor, density: torch.Tensor, 
+                  granularity=0.01):
+    memoryless_transparency = granularity*density
+    transparency = torch.exp(-torch.cumsum(memoryless_transparency, dim=-1))
+    volume = torch.sum(
+            transparency*(1 - torch.exp(-memoryless_transparency))*color_vec)
+
+    return volume 
 
